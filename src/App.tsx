732b3a4522b7f8d277deda165d24a1015eaa0dbf
Crișan Horea <h22ea@icloud.com>
2020-05-03 17:01:34 +0300

    Added Router 🚗

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import { useTypedSelector } from "./store/reducers";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 export const App = () => {
   const appConfig = useTypedSelector((state) => state.appConfig);
   console.log(appConfig);
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/home" exact>
+            <h1>Home</h1>
+          </Route>
+          <Route path="/test">
+            <h1>Test</h1>
+          </Route>
+          <Route>404</Route>
+        </Switch>
+      </Router>
+    </>
+  );
 };
