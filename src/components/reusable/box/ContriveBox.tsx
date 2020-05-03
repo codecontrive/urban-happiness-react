af333f9d10815558f1c469eb278bbd782e496f0c
Robert San <robertsandru96@gmail.com>
2020-05-03 21:21:26 +0300

    SCSS Config, React Bootstrap, Bootstrap & reusable

---
+++ src/components/reusable/box/ContriveBox.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import * as styles from "./ContriveBox.module.scss";
+
+export interface ContriveBoxProps {
+  roundedCorners: boolean;
+}
+
+export const ContriveBox = (props: ContriveBoxProps) => {
+  return <h1>box</h1>;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { App } from "./App";
 import * as serviceWorker from "./serviceWorker";
 import store from "./store";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/scss/global.scss";
 
 ReactDOM.render(
